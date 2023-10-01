8fe70ee05045ad4d0c4f295cd418d8dac4aa10e8
Carlos Chacon <carlos.flores@improvado.io>
2023-09-30 22:23:33 -0600

    Fixed linting issues

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,4 +1,4 @@
-""" 
+"""
 Test custom Django management commands.
 """
 
@@ -10,6 +10,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """ Test commands. """
@@ -20,14 +21,17 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
-    
+        patched_check.assert_called_once_with(databases=['default'])
+
     @patch('time.sleep')
-    def test_wait_for_db_delay(self,patched_sleep,patched_check):
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """ Test waiting for database when getting OperationalError. """
-        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True] #It shows 2 errors for db and 3 errors for connection, and later Success on the 6th time
+        # It shows 2 errs for db and 3 errs for connection,
+        # and later Success on the 6th time
+        patched_check.side_effect = [Psycopg2Error] * 2  \
+            + [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
-        self.assertEqual(patched_check.call_count,6)
-        patched_check.assert_called_with(database=['default'])
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
