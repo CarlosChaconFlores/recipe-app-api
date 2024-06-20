e8483241a9581647e44cbc0365a0d6974c374754
Carlos Chacon <carlos.flores@improvado.io>
2024-06-20 11:54:49 -0600

    Added tags and ingredients functionality

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,14 +1,14 @@
 """ Views for the recipe APIs """
 
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
-class RescipeViewSet(viewsets.ModelViewSet):
+class RecipeViewSet(viewsets.ModelViewSet):
     """ View for manage recipe APIs """
     serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
@@ -29,3 +29,33 @@ class RescipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """ Create a new recipe """
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
+    """ Manage tags in the database """
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """ Filter queryset for authenticated user """
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.DestroyModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    """ Manage ingredients in the database """
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """ Filter queryset to authenticated user """
+        return self.queryset.filter(user=self.request.user).order_by('-name')
